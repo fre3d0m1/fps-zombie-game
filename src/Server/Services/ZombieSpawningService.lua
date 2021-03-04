bb2b51e7c5f17cda4b90fba4109e5162fffffcc7
fre3d0m1 <63201933+fre3d0m1@users.noreply.github.com>
2021-03-04 16:16:14 -0500

    Create ZombieSpawningService.lua

---
+++ src/Server/Services/ZombieSpawningService.lua
@@ -0,0 +1,20 @@
+-- Zombie Spawning Service
+-- Akita
+-- March 4, 2021
+
+
+
+local ZombieSpawningService = {Client = {}}
+
+
+function ZombieSpawningService:Start()
+	
+end
+
+
+function ZombieSpawningService:Init()
+	
+end
+
+
+return ZombieSpawningService
